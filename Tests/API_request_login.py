112d2981ab4f529d4554c5aa8dc6c125c80484fe
miki <mikinagash1997@gmail.com>
2022-05-25 09:12:56 +0300

    Adding tests

--- Tests/API_request_login.py
+++ Tests/API_request_login.py
@@ -4,7 +4,7 @@ import requests
 class TestLogin:
         url = "https://trip-yoetz.herokuapp.com/auth/login"
 
-        def test_login_inorrect(self):
+        def test_login_with_invalid_details(self):
                  body = {'email': "mik@fsg", 'password': "177777723"}
                  x = requests.post(self.url ,data= body)
                  assert x.status_code == 400
@@ -17,11 +17,26 @@ class TestLogin:
                 print(x.status_code)
 
 
-        def test_login_inorrect_password(self):
+        def test_login_with_empty_password(self):
                 body = {'email': "mik", 'password': ""}
                 x = requests.post(self.url, data=body)
                 assert x.status_code == 400
                 print(x.status_code)
+
+
+        def test_login_correct(self):
+            body = {"email":"miki@gmail.com","password":"12345"}
+            x = requests.post(self.url,data= body)
+            assert x.status_code == 200
+            print(x.status_code)
+
+        def test_login_with_empty_fields(self):
+            body = {"email":"","password":""}
+            x = requests.post(self.url,data=body)
+            assert x.status_code == 400
+            print(x.status_code)
+
+
 
 
 
--- Tests/API_request_register.py
+++ Tests/API_request_register.py
@@ -1,1 +1,29 @@
+import requests
+import random
+class TestRegister:
+    url = "https://trip-yoetz.herokuapp.com/auth/register"
+
+    def test_register_correct(self):
+        body = {"birthDate": "2000-11-11",
+                "email": f"mikinagash@gmail.com{random.randint(1,100)}",
+                "image": "111",
+                "lastName": "nagash",
+                "name": "miki",
+                "password": "12345",
+                 "confirm password":"12345"}
+        x = requests.post(self.url,data=body)
+        print(x.status_code)
+        print(body.values())
+        assert x.status_code == 200
+
+
+    def test_register_with_exist_email (self):
+        body = {"email": "miki@gmail.com", "password": "12345", "birthDate": "2000-11-11", "name": "miki", "lastName": "nagash"}
+        x = requests.post(self.url, data=body)
+        assert x.status_code == 400
+        print(x.status_code)
+
+
+
+
 
